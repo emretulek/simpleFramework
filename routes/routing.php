9cc038be6581d440d50d12b4a7e5a9d02f306a4b
emretulek <memretulek@gmail.com>
2020-05-20 22:30:47 +0300

    Controller.php main method abstract yapıldı. Html.php source etiketi düzeltildi.

--- routes/routing.php
+++ routes/routing.php
@@ -1,6 +1,6 @@
 <?php
 
-//Router::any('/', "index@main")::name("index");
-//Router::get('/test', "index@main")->name("test");
-//Router::get('/test/{string}', "index@main")->name("test.test2");
-Router::autoRute();
+Router::any('/', "index@main")::name("index");
+Router::get('/test', "index@main")->name("test");
+Router::get('/test/{string}', "index@main")->name("test.test2");
+//Router::autoRute();

7377e0277be4e67bceee0a8fb49dd74246c28356
Emre Tülek <memretulek@gmail.com>
2021-04-22 22:08:50 +0300

    Version 3.5.0 SessionHandlers FileSessionHandler.php DatabaseSessionHandler.php RedisSessionHandler.php MemcachedSessionHandler.php Era.php date class QueryBuilder.php Fix Ve bir kaç küçük hata fix

--- routes/routing.php
+++ routes/routing.php
@@ -2,28 +2,3 @@
 
 Router::any('/', "home@index");
 
-Router::any('session1', function () {
-    Session::set('test', 10);
-    Session::set('test2', 15);
-    sleep(2);
-
-    dump($_SESSION);
-    echo 'session1';
-});
-
-Router::any('session2', function () {
-    Session::set('testç', 1);
-    Session::set('test3', 1);
-    dump($_SESSION);
-    sleep(4);
-    echo 'session2';
-});
-Router::any('session3', function () {
-
-    Session::set('registered', 1);
-    dump($_SESSION);
-    //sleep(3);
-    echo 'session3';
-});
-
-//session_destroy();
